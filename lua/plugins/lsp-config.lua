f823353147a907cf69bd2be2b3d1cbcb0a9b1d69
Quentin Garcia <quentingarcia40@gmail.com>
2025-01-29 12:11:58 +0100

    Add copy paste + tsconf deno

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -38,7 +38,35 @@ return {
 			lspConfig.remark_ls.setup({ capabilities = capabilities })
 			lspConfig.dockerls.setup({ capabilities = capabilities })
 			lspConfig.cssls.setup({ capabilities = capabilities })
-      require("typescript-tools").setup({})
+			lspConfig.denols.setup({
+				capabilities = capabilities,
+				root_dir = lspConfig.util.root_pattern("deno.json", "deno.jsonc"),
+			})
+			lspConfig.yamlls.setup({ capabilities = capabilities })
+
+			require("typescript-tools").setup({
+				capabilities = capabilities,
+				-- root_dir = function(fname)
+				-- 	local deno_root = lspConfig.util.root_pattern("deno.json", "deno.jsonc")(fname)
+				-- 	if deno_root then
+				-- 		return nil
+				-- 	end
+				--
+				-- 	-- INFO: stealed from:
+				-- 	-- https://github.com/neovim/nvim-lspconfig/blob/master/lua/lspconfig/server_configurations/tsserver.lua#L22
+				-- 	local root_dir = lspConfig.util.root_pattern("tsconfig.json")(fname)
+				-- 		or lspConfig.util.root_pattern("package.json", "jsconfig.json", ".git")(fname)
+				--
+				-- 	-- INFO: this is needed to make sure we don't pick up root_dir inside node_modules
+				-- 	local node_modules_index = root_dir and root_dir:find("node_modules", 1, true)
+				-- 	if node_modules_index and node_modules_index > 0 then
+				-- 		root_dir = root_dir:sub(1, node_modules_index - 2)
+				-- 	end
+				-- 	return root_dir
+				-- end,
+				-- single_file_support = false,
+			})
+
 			--      vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, {})
 			--     vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			--    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -7,7 +7,17 @@ vim.cmd("set ignorecase")
 vim.cmd("set smartcase")
 vim.g.mapleader = " "
 
-vim.api.nvim_set_keymap('n', '<C-D>', '<C-D>zz', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<C-U>', '<C-U>zz', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-D>", "<C-D>zz", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-U>", "<C-U>zz", { noremap = true, silent = true })
 
+-- Copy to clipboard
+vim.api.nvim_set_keymap("v", "<leader>y", '"+y', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>Y", '"+yg_', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>y", '"+y', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>yy", '"+yy', { noremap = true, silent = true })
 
+-- Paste from clipboard
+vim.api.nvim_set_keymap("n", "<leader>p", '"+p', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>P", '"+P', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "<leader>p", '"+p', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "<leader>P", '"+P', { noremap = true, silent = true })
