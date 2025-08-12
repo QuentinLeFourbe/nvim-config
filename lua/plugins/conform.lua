7aa1b0e895121db22bbea8705279faef5d4a5c90
Quentin Garcia <quentingarcia40@gmail.com>
2025-08-12 15:41:26 +0200

    update config: conform for format and move java and ts outside of lsp config file

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,42 +1,45 @@
 return {
-  "stevearc/conform.nvim",
-  dependencies = { "mason.nvim" },
-  cmd = "ConformInfo",
-  keys = {
-    {
-      "<leader>cf",
-      function()
-        require("conform").format({ async = true })
-      end,
-      desc = "Format buffer",
-      mode = { "n", "v" },
-    },
-  },
-  config = function()
-    require("conform").setup({
-      -- Define your formatters
-      formatters_by_ft = {
-        lua = { "stylua" },
-        css = { "prettierd" },
-        html = { "prettierd" },
-        javascript = { "prettierd" },
-        typescript = { "prettierd" },
-        typescriptreact = { "prettierd" },
-        javascriptreact = { "prettierd" },
-      },
+	"stevearc/conform.nvim",
+	dependencies = { "mason.nvim" },
+	cmd = "ConformInfo",
+	keys = {
+		{
+			"<leader>cf",
+			function()
+				require("conform").format({ async = true })
+			end,
+			desc = "Format buffer",
+			mode = { "n", "v" },
+		},
+	},
+	config = function()
+		require("conform").setup({
+			-- Define your formatters
+			formatters_by_ft = {
+				lua = { "stylua" },
+				css = { "prettierd" },
+				html = { "prettierd" },
+				javascript = { "prettierd" },
+				typescript = { "prettierd" },
+				typescriptreact = { "prettierd" },
+				javascriptreact = { "prettierd" },
+				xml = { "xmlformatter" },
+				yaml = { "yamlfmt" },
+				["*"] = { "prettierd" },
+			},
 
-      -- Set default options
-      default_format_opts = {
-        lsp_format = "fallback",
-      },
-      -- Set up format-on-save
-      format_on_save = { timeout_ms = 500 },
-      -- Customize formatters
-      formatters = {
-        shfmt = {
-          prepend_args = { "-i", "2" },
-        },
-      },
-    })
-  end,
+			-- Set default options
+			default_format_opts = {
+				lsp_format = "fallback",
+			},
+			-- Set up format-on-save
+			format_on_save = { timeout_ms = 500 },
+			-- Customize formatters
+			formatters = {
+				shfmt = {
+					prepend_args = { "-i", "2" },
+				},
+			},
+		})
+	end,
 }
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,75 +1,73 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = {
-          "lua_ls",
-          "html",
-          "graphql",
-          "jsonls",
-          "remark_ls",
-          "dockerls",
-          "cssls",
-        },
-      })
-    end,
-  },
-  {
-    "pmizio/typescript-tools.nvim",
-    dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-    opts = {},
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
-      local lspConfig = require("lspconfig")
-      lspConfig.lua_ls.setup({ capabilities = capabilities })
-      lspConfig.html.setup({ capabilities = capabilities })
-      lspConfig.graphql.setup({ capabilities = capabilities })
-      lspConfig.jsonls.setup({ capabilities = capabilities })
-      lspConfig.remark_ls.setup({ capabilities = capabilities })
-      lspConfig.dockerls.setup({ capabilities = capabilities })
-      lspConfig.cssls.setup({ capabilities = capabilities })
-      lspConfig.eslint.setup({
-        capabilities = capabilities,
-        on_attach = function(_, bufnr)
-          vim.api.nvim_create_autocmd("BufWritePre", {
-            buffer = bufnr,
-            command = "EslintFixAll",
-          })
-        end,
-      })
-      lspConfig.denols.setup({
-        capabilities = capabilities,
-        root_dir = lspConfig.util.root_pattern("deno.json", "deno.jsonc"),
-      })
-      lspConfig.yamlls.setup({ capabilities = capabilities })
+	{
+		"mason-org/mason.nvim",
+		opts = {},
+	},
+	{
+		"mason-org/mason-lspconfig.nvim",
+		opts = {
+			ensure_installed = {
+				"lua_ls",
+				"html",
+				"graphql",
+				"jsonls",
+				"remark_ls",
+				"dockerls",
+				"cssls",
+				"jdtls",
+			},
+		},
+		dependencies = {
+			{ "mason-org/mason.nvim", opts = {} },
+			"neovim/nvim-lspconfig",
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			require("java").setup()
+			local lspConfig = require("lspconfig")
+			lspConfig.jdtls.setup({ capabilities = capabilities })
+			lspConfig.lua_ls.setup({ capabilities = capabilities })
+			lspConfig.html.setup({ capabilities = capabilities })
+			lspConfig.graphql.setup({ capabilities = capabilities })
+			lspConfig.jsonls.setup({ capabilities = capabilities })
+			lspConfig.remark_ls.setup({ capabilities = capabilities })
+			lspConfig.dockerls.setup({ capabilities = capabilities })
+			lspConfig.cssls.setup({ capabilities = capabilities })
+			lspConfig.eslint.setup({
+				capabilities = capabilities,
+				on_attach = function(_, bufnr)
+					vim.api.nvim_create_autocmd("BufWritePre", {
+						buffer = bufnr,
+						command = "EslintFixAll",
+					})
+				end,
+			})
+			lspConfig.denols.setup({
+				capabilities = capabilities,
+				root_dir = lspConfig.util.root_pattern("deno.json", "deno.jsonc"),
+			})
+			lspConfig.yamlls.setup({ capabilities = capabilities })
 
-      require("typescript-tools").setup({
-        capabilities = capabilities,
-      })
-    end,
-    keys = {
-      {
-        "<leader>ca",
-        vim.lsp.buf.code_action,
-        desc = "Code action",
-        mode = { "n", "v" },
-      },
-      {
-        "<leader>cr",
-        vim.lsp.buf.rename,
-        desc = "Code rename",
-        mode = { "n" },
-      },
-    },
-  },
+			require("typescript-tools").setup({
+				capabilities = capabilities,
+			})
+		end,
+		keys = {
+			{
+				"<leader>ca",
+				vim.lsp.buf.code_action,
+				desc = "Code action",
+				mode = { "n", "v" },
+			},
+			{
+				"<leader>cr",
+				vim.lsp.buf.rename,
+				desc = "Code rename",
+				mode = { "n" },
+			},
+		},
+	},
 }
